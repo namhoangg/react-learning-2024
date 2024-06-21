82ced3b5d4129abb3e8f13e53dcadfa9089be23a
Hoang Dai Nam <nam.hoang-dai@banvien.com.vn>
2024-06-21 15:27:20 +0700

    prj6

--- Prj6/src/components/Input.jsx
+++ Prj6/src/components/Input.jsx
@@ -1,13 +1,20 @@
-export default function Input({label,textarea,...props}) {
-    const className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
-    return (
-        <>
-            <p className="flex flex-col gap-1 my-4">
-            <label className="text-sm font-bold uppercase text-stone-500">
-                {label}
-            </label>
-            {textarea ? <textarea className={className} {...props}/> : <input className={className} {...props}/>}
-            </p>
-        </>
-    )
-}  
+import { forwardRef } from "react";
+const Input = forwardRef(function Input({ label, textarea, ...props },ref) {
+  const className =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+  return (
+    <>
+      <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500">
+          {label}
+        </label>
+        {textarea ? (
+          <textarea ref={ref} className={className} {...props} />
+        ) : (
+          <input  ref={ref} className={className} {...props} />
+        )}
+      </p>
+    </>
+  );
+});
+export default Input;
--- Prj6/src/components/NewProject.jsx
+++ Prj6/src/components/NewProject.jsx
@@ -1,7 +1,21 @@
 import Input from "./Input";
 import {useRef,useState} from 'react';
-export default function NewProject() {
-  
+export default function NewProject({onAdd}) {
+  const title=useRef(null);
+  const description=useRef(null);
+  const dueDate=useRef(null);
+  function handleSave(){
+    const enteredTitle=title.current.value;
+    const enteredDescription=description.current.value;
+    const enteredDueDate=dueDate.current.value;
+    //..validation
+
+    onAdd({
+      title:enteredTitle,
+      description:enteredDescription,
+      dueDate:enteredDueDate,
+    })
+  }
   return (
     <>
       <div className="w-[35rem] mt-16">
@@ -12,15 +26,15 @@ export default function NewProject() {
             </button>
           </li>
           <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
             Save
           </button>
           </li>
         </menu>
         <div>
-            <Input label="Title"/>
-            <Input label="Description" textarea/>
-            <Input label="Due Date"/>
+            <Input ref={title} label="Title"/>
+            <Input ref={description} label="Description" textarea/>
+            <Input ref={dueDate}  label="Due Date"/>
         </div>
       </div>
     </>
